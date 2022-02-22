3475d07652d4029b74427302e457e7cc430c3f59
the5gi <86175493+the5gi@users.noreply.github.com>
2022-02-22 08:16:06 -0500

    Update Main File (No Release)

--- cmd.cmd
+++ cmd.cmd
@@ -39,6 +39,31 @@ echo.
 echo.
 echo Please Choose a mod to install. (Will automatically install dependencies if there are any)
 echo.
+echo 1. Airjump
+echo 2. Broken Controller
+echo 3. Computer Interface
+echo 4. Cosmetic Hider
+echo 5. Dash Monke
+echo 6. Gorilla Cosmetics
+echo 7. Monke Map Loader
+echo 8. Monke Swim
+echo 9. Monkey Trails
+echo 10. No Ambient
+echo 11. Power Arms
+echo 12. Practice Mod
+echo 13. Slippery Ice
+echo 14. Space Monke
+echo 15. Spectator Mod
+echo 16. YURFitGorillaTag
+echo --------------------------
+echo          DEPENDENCIES
+echo --------------------------
+echo 17. Bannana Hook
+echo 18. BepInject
+echo 19. Extenject
+echo 20. TMPLoader
+echo 21. Utilla
+echo.
 set /p csmi=" Mod to Install >>> "
 if '%csmi%'=='1' echo yes
 if '%csmi%'=='2' echo yes
@@ -52,5 +77,14 @@ if '%csmi%'=='9' echo yes
 if '%csmi%'=='10' echo yes
 if '%csmi%'=='11' echo yes
 if '%csmi%'=='12' echo yes
+if '%csmi%'=='13' echo yes
+if '%csmi%'=='14' echo yes
+if '%csmi%'=='15' echo yes
+if '%csmi%'=='16' echo yes
+if '%csmi%'=='17' echo yes
+if '%csmi%'=='18' echo yes
+if '%csmi%'=='19' echo yes
+if '%csmi%'=='20' echo yes
+if '%csmi%'=='21' echo yes
 pause
 goto home
